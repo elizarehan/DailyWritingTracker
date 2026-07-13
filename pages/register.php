aa71594d6fd5de34d9eb7ae2eb481794dd2af49f
Eliza <130762180+elizarehan@users.noreply.github.com>
2026-07-13 14:58:28 -0400

    Update register.php

--- pages/register.php
+++ pages/register.php
@@ -1,11 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+<head>
     <title>Create Account</title>
     <link rel="stylesheet" href="../assets/css/styles.css">
 </head>
 
 <body>
-
-  
+    <h1>Create an account</h1>
+    <form id="registerForm" action="../database/insertUser.php" method="POST">
+        <label for="email">Email: </label>
+        <input
+            name="email"
+            type="email"
+            required
+        />
+        <label for="password">Password: </label>
+        <input
+            name="password"
+            type="password"
+            required
+        />
+            
 </body>
+
+</html>
