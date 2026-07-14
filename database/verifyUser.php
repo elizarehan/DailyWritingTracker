8cbf4e83190149cdfc836d89b71517c349455b99
Eliza <130762180+elizarehan@users.noreply.github.com>
2026-07-14 12:40:58 -0400

    Create verifyUser.php

---
+++ database/verifyUser.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+require_once "dbConnect.php";
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$stmt = $conn->prepare("SELECT * FROM userdata WHERE email = ? LIMIT 1);
+$stmt->bindparam("s", $email);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if (result->num_rows != 1) {
+  echo "Account not found";
+  exit;
+}
+
+$row = $result->fetch_assoc();
+
+if (!password_verify($password, $row['password'])) {
+  echo "Incorrect password";
+}
+
+echo "OK";
+exit;
+?>
