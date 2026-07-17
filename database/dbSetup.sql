829e9f6202f72426c31b6109be9264d931635f0d
Eliza <130762180+elizarehan@users.noreply.github.com>
2026-07-17 09:56:33 -0400

    Update dbSetup.sql

--- database/dbSetup.sql
+++ database/dbSetup.sql
@@ -1,6 +1,20 @@
 CREATE TABLE userdata (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(45),
-    email VARCHAR(45),
+    name NVARCHAR(50),
+    email VARCHAR(255),
     password VARCHAR(255),
+);
+
+CREATE TABLE projects (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    form VARCHAR(50),
+    curr_words INT NOT NULL CHECK (word_count >= 0),
+    goal_words INT NOT NULL CHECK (word_count >= 0),
+    deadline DATE NULL,
+    description VARCHAR(max),
+    draft_link NVARCHAR(2083) NULL,
+    genre VARCHAR(50),
+    draft_num VARCHAR(20),
+    notes VARCHAR(255)
 );
